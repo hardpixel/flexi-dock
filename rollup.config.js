d176eae2f46c3fafafdc277f2dfd1b7a5274669b
jonian <jonian@hardpixel.eu>
2022-04-18 12:40:10 +0300

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import extension from 'rollup-plugin-gsext'
 import sass from 'rollup-plugin-sass'
+import del from 'rollup-plugin-delete'
 
 export default {
   input: [
@@ -14,6 +15,12 @@ export default {
     extension(),
     sass({
       output: 'dist/stylesheet.css'
+    }),
+    del({
+      hook: 'writeBundle',
+      targets: [
+        'dist/stylesheet.js'
+      ]
     })
   ]
 }
