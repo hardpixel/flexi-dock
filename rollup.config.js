9aade07c31a14ea823961702590cf3feffbdb36a
jonian <jonian@hardpixel.eu>
2023-05-27 12:47:44 +0300

    add package script support

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,9 @@
+import pkg from './package.json'
+
 import extension from 'rollup-plugin-gsext'
 import sass from 'rollup-plugin-sass'
 import del from 'rollup-plugin-delete'
+import zip from 'rollup-plugin-zipdir'
 
 export default {
   input: [
@@ -12,15 +15,23 @@ export default {
     format: 'esm'
   },
   plugins: [
-    extension(),
+    extension({
+      metadata: {
+        version: Number(pkg.version)
+      }
+    }),
     sass({
       output: 'dist/stylesheet.css'
     }),
     del({
       hook: 'writeBundle',
       targets: [
-        'dist/stylesheet.js'
+        'dist/stylesheet.js',
+        'build/*.zip'
       ]
+    }),
+    process.env.package && zip({
+      outputDir: 'build'
     })
   ]
 }
